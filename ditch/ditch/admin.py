44b1c7e1bc01797ca1a52257f7c863db09e8020b
Ed Henderson <ed@eeweb.com>
2014-06-11 12:37:29 -0600

    Resolve editable issues in Admin.

--- ditch/ditch/admin.py
+++ ditch/ditch/admin.py
@@ -1,15 +1,20 @@
 
 from django.contrib import admin as a
 from django.views.decorators.cache import never_cache
-
+from django.contrib.auth.models import User,Group
+from ditchdb.models import DitchLog,DitchCal
 
 class DitchAdminSite(a.AdminSite):
     @never_cache
     def index(self, request, extra_context=None):
-        context = {'title': 'Schematics.com Administration'}
+        context = {'title': 'ditch.gardenbuzz.com Administration'}
         for model, model_admin in self._registry.items():
             setattr(model._meta, 'verbose_name_plural', unicode(model._meta.verbose_name_plural))
         response = super(DitchAdminSite, self).index(request, extra_context=context)
         return response
 
 site = DitchAdminSite()
+site.register(User)
+site.register(Group)
+site.register(DitchLog)
+site.register(DitchCal)
