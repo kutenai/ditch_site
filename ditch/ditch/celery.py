6f743ff846abc52d52f9f6e73f8e89e732f920dd
Ed Henderson <ed@eeweb.com>
2014-06-13 18:29:35 -0600

    Okay, I don't need to import that like I thought.. it was a cache issue.

--- ditch/ditch/celery.py
+++ ditch/ditch/celery.py
@@ -6,8 +6,6 @@ from django.conf import settings
 
 app = Celery()
 
-from ditchtasks.tasks import status
-
 # Using a string here means the worker will not have to
 # pickle the object when using Windows.
 app.config_from_object('django.conf:settings')
